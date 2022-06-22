92cd8257a70b0dd06f614abfb5e07c360d1ff02c
SinoharaRyuga <raitan3170@gmail.com>
2022-06-22 22:11:33 +0900

    一定時間後にノーマライズ出来るようにした

--- Assets/TrailRenderer/HomingBullet/Bullet.cs
+++ Assets/TrailRenderer/HomingBullet/Bullet.cs
@@ -1,18 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
     [SerializeField] float _speed = 3f;
+    [SerializeField] float _normalizedTime = 1f;
     [SerializeField] float _correctionStrength = 1f;
+    [SerializeField, Tooltip("true=ノーマライズする　false=ノーマライズを止める")] bool _isChangeNormalized = false;
     Rigidbody2D _rb2D => GetComponent<Rigidbody2D>();
     Transform _enemyTransform = default;
     Vector3 _dirction = Vector3.zero;
+    bool _isnormalized = false;
 
     private void Start()
     {
-        // Destroy(gameObject, 3f);
         _enemyTransform = EnemyGenerator.GetRamdomEnemy();
+        _isChangeNormalized = !_isChangeNormalized;
+        StartCoroutine(ChangeNormalized());
     }
 
     private void Update()
@@ -20,6 +25,8 @@ public class Bullet : MonoBehaviour
         if (_enemyTransform == null)
         {
             _enemyTransform = EnemyGenerator.GetRamdomEnemy();
+            _isChangeNormalized = !_isChangeNormalized;
+            StartCoroutine(ChangeNormalized());
         }
 
         BulletMove();
@@ -31,7 +38,7 @@ public class Bullet : MonoBehaviour
         {
             Destroy(collision.gameObject);
             EnemyGenerator.RemoveEnemy(collision.transform);
-            //Destroy(gameObject);
+            // Destroy(gameObject);
         }
     }
 
@@ -42,9 +49,25 @@ public class Bullet : MonoBehaviour
 
     public void BulletMove()
     {
-        Vector3 cor = (_enemyTransform.position - transform.position).normalized * _correctionStrength * Time.deltaTime;
-        _dirction = (_dirction + cor).normalized;
-        transform.position += _dirction * _speed * Time.deltaTime;
+        if (_isnormalized)
+        {
+            Vector3 cor = (_enemyTransform.position - transform.position).normalized * _correctionStrength * Time.deltaTime;
+            _dirction = (_dirction + cor).normalized;
+            transform.position += _dirction * _speed * Time.deltaTime;
+        }
+        else
+        {
+            Vector3 cor = (_enemyTransform.position - transform.position).normalized * _correctionStrength * Time.deltaTime;
+            _dirction = (_dirction + cor);
+            transform.position += _dirction * _speed * Time.deltaTime;
+        }
+    }
+
+    /// <summary>一定時間後に設定した処理を行う </summary>
+    IEnumerator ChangeNormalized()
+    {
+        yield return new WaitForSeconds(_normalizedTime);
+        _isnormalized = _isChangeNormalized;
     }
 }
 
